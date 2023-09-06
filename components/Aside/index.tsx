68737dd0e36114eb51b8275b3010ebdd47a85f8d
S.Mohammad Hosseininejad <smohammadhnd@gmail.com>
2023-09-06 17:06:48 +0330

    feat: setting up layout responsiveness

--- components/Aside/index.tsx
+++ components/Aside/index.tsx
@@ -12,7 +12,7 @@ export default function Aside() {
   const color = 'secondary'
 
   return (
-    <aside className="aside">
+    <aside className="aside invisible">
       <div className="aside--container">
         <div className="aside__heading">
           <Link href="/" className="aside__heading--logo">
